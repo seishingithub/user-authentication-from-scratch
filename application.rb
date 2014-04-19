f1186b19ffa84d4bb682dff7b53bca67d80059a7
Peggy Griffin <seishingithub@gmail.com>
2014-04-19 00:39:32 -0600

    [#69804784] Added CSS styling to site

--- application.rb
+++ application.rb
@@ -5,11 +5,6 @@ class Application < Sinatra::Application
 
   def initialize(app=nil)
     super(app)
-
-    # initialize any other instance variables for you
-    # application below this comment. One example would be repositories
-    # to store things in a database.
-
   end
 
   get '/' do
